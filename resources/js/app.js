7bdc05a1d6a3535d0e9a559cea6d291cb27b0859
piamelgarejo <129460206+piamelgarejo@users.noreply.github.com>
2024-07-17 12:25:55 -0300

    actualización aside

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Archivo: resources/js/app.js
+import './bootstrap';
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
